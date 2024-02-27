19a397fd55e5ac89e421632de969b13a96f5aa3f
DhanushM12 <dhanushmukhi@gmail.com>
2024-02-27 21:45:00 +0530

    27th feb 1st

--- introtojs/index.js
+++ introtojs/index.js
@@ -52,6 +52,7 @@
 
 // console.log(typeof(abc[0]));
 
+// let arr = ["abdb", 17, true, {"abc": "smbsjb"}];
 
 // var, let, const
 
@@ -115,16 +116,87 @@
 
 
 // all the parameters of the function are locally scoped cannot be accessed outside the function
-var ax = "a"
-function sum(a, b){
-    let ax;
-    // console.log(c);
-    let result = a+b;
-    console.log(result)
-    ax = "lldldm"
-    console.log(ax)
+// var ax = "a"
+// function sum(a, b){
+//     let ax;
+//     // console.log(c);
+//     let result = a+b;
+//     console.log(result)
+//     ax = "lldldm"
+//     console.log(ax)
+// }
+// ax = "b"
+// sum(3, 4, ax);
+// console.log(ax);
+
+
+// loops ?
+// var i =1; // assignment
+// while (i<=5) { // condition
+//     console.log(i);
+//     i=i+1; // increment
+    
+// }
+
+// for(var i=1;i <=5;i++){
+//     console.log(i)
+// }
+
+
+// event listner way
+// document.getElementById('btn').addEventListener('click', getDate)
+// this will give me the current date and time
+function getDate(){
+    let date = Date();
+    console.log(date);
+    document.getElementById('display-date').innerHTML = "<h1>hello people current date and time is: " + date + "</h1>";
+
+    // `<h1>hello people current date and time is: ${date}</h1>`
+    document.getElementById('myimg').innerHTML = `<img src="https://media.istockphoto.com/id/1672317574/photo/ama-dablam-mountain-peak.webp?b=1&s=170667a&w=0&k=20&c=Ea8yDEHpUemrRuMZUKGPDBE11YTWVksIupMN8FkEBf8=" alt="" srcset="">`;
 }
-ax = "b"
-sum(3, 4, ax);
-console.log(ax);
+
+function saveName(e){
+    e.preventDefault();
+   let name=  document.getElementById("myname").value;
+//    console.log(name);
+   let len =name.length;
+//    console.log(len);
+//     let checka = name.charAt(len-9-1);
+//     console.log(checka)
+    console.log(name.charAt(0));
+    // console.log(name.join(' '));
+//     console.log(name.at(1));
+//     console.log(name[2]);
+//     console.log(name.toUpperCase());
+    let outputName = "";
+    name.charAt(0).toUpperCase();
+    console.log(name);
+    for(let i=1;i<len;i++){
+        let mychar = name.charAt(i);
+        if(mychar == " "){
+            if(i+1<len) {
+                mychar = mychar + name.charAt(i+1).toUpperCase();
+                i=i+1;
+            }
+        }
+        outputName = outputName + mychar;
+        
+    }
+    console.log(outputName);
+    // document.getElementById('print-name').innerText = name + " batch name";
+}
+
+// `${variableName}`
+// 0 
+// @gmail.com
+// 12345678910
+
+// dm@gmail.com
+
+// kdnkdnkd@gmail.com
+
+// sknkdnkndkdklnkdnkdn@gmail.com
+
+
+
 
